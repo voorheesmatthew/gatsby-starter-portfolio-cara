7a983611248fa4cd2952de33b3db5816c3bff61d
Matt Voorhees <voorhees.matthew@gmail.com>
2020-06-10 17:40:08 -0700

    remove box

--- src/@lekoarts/gatsby-theme-cara/components/footer.jsx
+++ src/@lekoarts/gatsby-theme-cara/components/footer.jsx
@@ -2,23 +2,23 @@
 import { Box, useColorMode, jsx } from "theme-ui"
 
 const Footer = () => {
-    const [colorMode, setColorMode] = useColorMode()
-    const isDark = colorMode === `dark`
-    const toggleColorMode = (e, any) => {
-        setColorMode(isDark ? `light` : `dark`)
+    // const [colorMode, setColorMode] = useColorMode()
+    // const isDark = colorMode === `dark`
+    // const toggleColorMode = (e, any) => {
+    //     setColorMode(isDark ? `light` : `dark`)
     }
 
     return (
-        <Box as="footer" variant="footer">
-            <button
-                sx={{ variant: `buttons.toggle`, fontWeight: `semibold`, display: `block`, mx: `auto`, mb: 3 }}
-                onClick={toggleColorMode}
-                type="button"
-                aria-label="Toggle dark mode"
-            >
-                {isDark ? `Light` : `Dark`}
-            </button>
-        </Box>
+        // <Box as="footer" variant="footer">
+        //     <button
+        //         sx={{ variant: `buttons.toggle`, fontWeight: `semibold`, display: `block`, mx: `auto`, mb: 3 }}
+        //         onClick={toggleColorMode}
+        //         type="button"
+        //         aria-label="Toggle dark mode"
+        //     >
+        //         {isDark ? `Light` : `Dark`}
+        //     </button>
+        // </Box>
     )
 }
 
